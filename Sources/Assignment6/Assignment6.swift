1f5e15b99ed15f6fa884d9ef3513debae7496c91
John Potz <jthpotz@gmail.com>
2019-03-03 17:26:33 -0800

    More types and tests

--- Sources/Assignment6/Assignment6.swift
+++ Sources/Assignment6/Assignment6.swift
@@ -1,4 +1,23 @@
 class ExprC {}
+class Value {}
+
+struct EnvStruct {
+    let id : String
+    let val : Value
+    
+    init (id : String, val : Value) {
+        self.id = id
+        self.val = val
+    }
+}
+
+class Env {
+    let list : [EnvStruct]
+    
+    init (list : [EnvStruct]) {
+        self.list = list
+    }
+}
 
 class NumC : ExprC{
     let num : Float
@@ -14,5 +33,90 @@ class StrC : ExprC {
     
     init (str : String) {
         self.str = str
+    }
+}
+
+
+class IdC : ExprC {
+    let id : String
+    
+    init (id : String) {
+        self.id = id
+    }
+}
+
+class AppC : ExprC {
+    let fn : ExprC
+    let args : [ExprC]
+    
+    init (fn : ExprC, args : [ExprC]) {
+        self.fn = fn
+        self.args = args
+    }
+}
+
+class IfC : ExprC {
+    let ifStmnt : ExprC
+    let thenStmnt : ExprC
+    let elseStmnt : ExprC
+    
+    init (ifStmnt : ExprC, thenStmnt : ExprC, elseStmnt : ExprC) {
+        self.ifStmnt = ifStmnt
+        self.thenStmnt = thenStmnt
+        self.elseStmnt = elseStmnt
+    }
+}
+
+class LamC : ExprC {
+    let param : [ExprC]
+    let body : ExprC
+    
+    init (param : [ExprC], body : ExprC) {
+        self.param = param
+        self.body = body
+    }
+}
+
+class NumV : Value {
+    let num : Float
+    
+    init (num : Float) {
+        self.num = num
+    }
+}
+
+class StrV : Value {
+    let str : String
+    
+    init (str : String) {
+        self.str = str
+    }
+}
+
+class CloV : Value {
+    let param : [IdC]
+    let body : ExprC
+    let cloEnv : Env
+    
+    init (param : [IdC], body : ExprC, cloEnv : Env) {
+        self.param = param
+        self.body = body
+        self.cloEnv = cloEnv
+    }
+}
+
+class BoolV : Value {
+    let b : Bool
+    
+    init (b : Bool) {
+        self.b = b
+    }
+}
+
+class PrimV : Value {
+    let fn : (([Value]) -> Value)
+    
+    init (fn : @escaping (([Value]) -> Value)) {
+        self.fn = fn
     }
 }
--- Tests/Assignment6Tests/Assignment6Tests.swift
+++ Tests/Assignment6Tests/Assignment6Tests.swift
@@ -11,9 +11,66 @@ final class Assignment6Tests: XCTestCase {
         let s = StrC(str: "hi")
         XCTAssertEqual(s.str, "hi")
     }
+    
+    func testInitIdC() {
+        let id = IdC(id: "hi")
+        XCTAssertEqual(id.id, "hi")
+    }
+    
+    func testInitAppC() {
+        let app = AppC(fn: NumC(num: 1), args: [NumC(num: 1)])
+        XCTAssertEqual((app.fn as! NumC).num, 1)
+    }
+    
+    func testInitIfC() {
+        let f = IfC(ifStmnt: NumC(num: 1), thenStmnt: NumC(num: 1), elseStmnt: NumC(num: 2))
+        XCTAssertEqual(((f.ifStmnt as! NumC).num == 1) && ((f.thenStmnt as! NumC).num == 1), true)
+    }
+    
+    func testInitLamC() {
+        let l = LamC(param: [NumC(num: 1)], body: NumC(num: 1))
+        XCTAssertEqual((l.param[0] as! NumC).num, 1)
+    }
+
+    func testInitNumV() {
+        let n = NumV(num: 1)
+       XCTAssertEqual(n.num, 1)
+    }
+    
+    func testInitStrV() {
+        let s = StrV(str: "hi")
+       XCTAssertEqual(s.str, "hi")
+    }
+    
+    func testInitCloV() {
+        let c = CloV(param: [IdC(id: "hi")], body: NumC(num: 1), cloEnv: Env(list: []))
+       XCTAssertEqual(((c.param[0].id == "hi") && ((c.body as! NumC).num == 1)), true)
+    }
+    
+    func testInitBoolV() {
+        let b = BoolV(b: true)
+       XCTAssertEqual(b.b, true)
+    }
+    
+    func testInitPrimV() {
+        func testPrimFunc (vals: [Value]) -> Value {
+            return vals[0]
+        }
+        let p = PrimV(fn: testPrimFunc)
+       XCTAssertEqual((p.fn([NumV(num: 1)]) as! NumV).num, 1)
+    }
 
     static var allTests = [
         ("testInitNumC", testInitNumC),
         ("testInitStrC", testInitStrC),
+        ("testInitIdC", testInitIdC),
+        ("testInitAppC", testInitAppC),
+        ("testInitIfC", testInitIfC),
+        ("testInitLamC", testInitLamC),
+        ("testInitNumV", testInitNumV),
+        ("testInitStrV", testInitStrV),
+        ("testInitCloV", testInitCloV),
+        ("testInitBoolV", testInitBoolV),
+        ("testInitPrimV", testInitPrimV)
     ]
 }
